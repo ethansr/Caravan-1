3c6e09cdf145b1dc2662d848b542e8f2af43bae3
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2014-02-18 12:41:09 -0800

    Water Engine mostly working

---
+++ Assets/Scripts/Bucket.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bucket : MonoBehaviour {
+	public Player player;
+	private PlayerInventory inventory;
+	private Vector3 original_position;
+	// Use this for initialization
+	void Start () {
+		inventory = player.GetComponent<PlayerInventory> ();
+		original_position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position =  original_position + Vector3.down * inventory.wellDepth * 7;
+	}
+}
---
+++ Assets/Scripts/WaterMine.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterMine : DropLocation {
+
+	public override void SetOccupant (GameObject o)
+	{ 
+		Meeple meeple = o.GetComponent <Meeple>();
+		GameObject player = meeple.player;
+		PlayerInventory inventory = player.GetComponent<PlayerInventory> ();
+		inventory.wellDepth += 1;
+		base.SetOccupant (o);
+	}
+}
