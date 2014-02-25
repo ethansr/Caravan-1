464305b0478626fef881c46a38de8b712be2932a
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2014-02-25 14:36:32 -0800

    Prefab for MerchantCard

---
+++ Assets/MerchantCard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MerchantCard : MonoBehaviour {
+	public DesertGenerator.GoodItem
+	public GameObject first_position;
+	public GameObject second_position;
+	public GameObject third_position;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/TradePlacement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TradePlacement : DropLocation {
+	public DesertGenerator.GoodItem good;
+
+	// Use this for initialization
+	void Start () {
+		GameObject desert = GameObject.Find("Desert");
+
+		GetComponent<SpriteRenderer> ().sprite = desert.GetComponent<DesertTileIndex> ().goodTileSprites [(int)good];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
