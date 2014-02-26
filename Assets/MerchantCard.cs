4323d8e6cc9ac2cfd4834a518759947522811e40
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2014-02-25 16:39:17 -0800

    Merchant card generation

--- Assets/MerchantCard.cs
+++ Assets/MerchantCard.cs
@@ -10,16 +10,23 @@ public class MerchantCard : MonoBehaviour {
 	public GameObject third_position;
 	// Use this for initialization
 	void Start () {
+		SetGoods (first_good, second_good, third_good);
+	}
+
+	public void SetGoods(DesertGenerator.GoodItem first, DesertGenerator.GoodItem second, DesertGenerator.GoodItem third) {
+		first_good = first;
+		second_good = second;
+		third_good = third;
+
 		first_position.GetComponent<TradePlacement>().setGood(first_good);
 		second_position.GetComponent<TradePlacement>().setGood(second_good);
-		if (third_good == null) {
+		if (third_good == (DesertGenerator.GoodItem)(-1)) {
 			Destroy (third_position);
 		} else {
 			third_position.GetComponent<TradePlacement>().setGood(third_good); 
 		}
-		
+
 	}
-	
 	// Update is called once per frame
 	void Update () {
 	
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
@@ -7,12 +8,68 @@ public class GameController : MonoBehaviour {
 	public int indexOfNextPlayer;
 	public static int numPlayers = 4;
 	public static int numMeeplesPerPlayer=5;
+	public Stack<GameObject> merchantCards = new Stack<GameObject>();
+
+	public GameObject merchant_card;
 
 	// Use this for initialization
 	void Start () {
 	
+		BuildDeck ();
+
 		//for testing
 		gameObject.GetComponent<DesertMovementController> ().beginDesertMovementPhase ();
+
+
+
+
+	}
+
+	void BuildDeck() {
+		int numberOfTwoGoodCards = 24;
+		int numberOfThreeGoodCards = 24;
+		
+		for (int i = 0; i < numberOfTwoGoodCards; i++) {
+			int first_type = Random.Range(0,3);
+			int second_type;
+			
+			do {
+				second_type = Random.Range(0,3);
+			} while(first_type == second_type);
+			
+			int first_good = first_type * 4 + Random.Range(0,3);
+			int second_good = second_type * 4 + Random.Range(0,3);
+			GameObject card = (GameObject)Instantiate(merchant_card);
+			card.GetComponent<MerchantCard>().SetGoods((DesertGenerator.GoodItem)first_good,(DesertGenerator.GoodItem)second_good, (DesertGenerator.GoodItem)(-1));
+			card.transform.position = card.transform.position + Vector3.left * i * 8 + Vector3.right * 45;
+
+
+			merchantCards.Push (card);
+			
+		}
+
+		for (int i = 0; i < numberOfThreeGoodCards; i++) {
+			int first_type = Random.Range(0,3);
+			int second_type;
+			int third_type;
+			
+			do {
+				second_type = Random.Range(0,3);
+			} while(first_type == second_type);
+			do {
+				third_type = Random.Range(0,3);
+			} while(first_type == second_type || second_type == third_type || first_type == third_type );
+			
+			int first_good = first_type * 4 + Random.Range(0,3);
+			int second_good = second_type * 4 + Random.Range(0,3);
+			int third_good = third_type * 4 + Random.Range (0,3);
+			GameObject card = (GameObject)Instantiate(merchant_card);
+			card.GetComponent<MerchantCard>().SetGoods((DesertGenerator.GoodItem)first_good,(DesertGenerator.GoodItem)second_good, (DesertGenerator.GoodItem)third_good);
+			card.transform.position = card.transform.position + Vector3.left * i * 8 + Vector3.right * 45 ;
+			card.transform.position = card.transform.position + Vector3.down * 16;
+			merchantCards.Push (card);
+			
+		}
 
 	}
 	
