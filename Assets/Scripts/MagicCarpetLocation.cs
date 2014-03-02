22c9bfddf139eefbc60263dbb3de7666d8384cb9
ecramer89 <ecramer89@gmail.com>
2014-03-02 13:52:35 -0800

    some changes to magic carpet; should accept a good item now...

--- Assets/Scripts/MagicCarpetLocation.cs
+++ Assets/Scripts/MagicCarpetLocation.cs
@@ -3,34 +3,46 @@ using System.Collections;
 
 public class MagicCarpetLocation : DropLocation
 {
+		GameObject goodToTradeForCarpet;
 
 		// Use this for initialization
 		void Start ()
 		{
 				allowMultipleOccupants = false;
-		
 		}
 
 		protected override bool CanOccupy (GameObject potentialOccupant)
-		{
+		{       
 				bool playerHasGoods = potentialOccupant.GetComponent<Meeple> ().player.GetComponent<PlayerInventory> ().hasGoods ();
-				Debug.Log (playerHasGoods);
-				return (base.CanOccupy (potentialOccupant) && true);//playerHasGoods);
+				
+				if (isMeeple (potentialOccupant))
+						return (base.CanOccupy (potentialOccupant) && playerHasGoods);
+				else
+						return (!goodToTradeForCarpet && potentialOccupant.CompareTag ("goodToken"));
 		}
 	
 		public override void SetOccupant (GameObject o)
 		{
-				base.SetOccupant (o);
-				if (o) {
-						MagicCarpet.playerWithMagicCarpet = o.GetComponent<Meeple> ().player;
-						//DesertMovementController.playerWithMagicCarpet = o.GetComponent<Meeple> ().player;
-						GameObject.Find ("GameController").GetComponent<GameController> ().getNextPlayer ();
+				if (isMeeple (o))
+						base.SetOccupant (o);
+				else
+						goodToTradeForCarpet = o;
 
+				if (occupant && goodToTradeForCarpet) {
+						MagicCarpet.playerWithMagicCarpet = o.GetComponent<Meeple> ().player;
+						//goodToTradeForCarpet.DestroyFOREVER!!!!!
+						GameObject.Find ("GameController").GetComponent<GameController> ().getNextPlayer ();
+			
 			
 				}
 		
 		
 		
+		}
+
+		bool isMeeple (GameObject potentialOccupant)
+		{
+				return potentialOccupant.GetComponent<Meeple> ();
 		}
 
 		bool playerHasAnyGoods (GameObject meeple)
