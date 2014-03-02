008f70222b39757f21d97a90572ceb71965fb8ed
ecramer89 <ecramer89@gmail.com>
2014-03-02 14:42:31 -0800

    magic carpet good drag works; destroys good sprite after payment

--- Assets/Scripts/MagicCarpetLocation.cs
+++ Assets/Scripts/MagicCarpetLocation.cs
@@ -9,39 +9,52 @@ public class MagicCarpetLocation : DropLocation
 		void Start ()
 		{
 				allowMultipleOccupants = false;
+				goodToTradeForCarpet = null;
 		}
 
 		protected override bool CanOccupy (GameObject potentialOccupant)
 		{       
-				bool playerHasGoods = potentialOccupant.GetComponent<Meeple> ().player.GetComponent<PlayerInventory> ().hasGoods ();
 				
-				if (isMeeple (potentialOccupant))
+				
+				if (isMeeple (potentialOccupant)) {
+						bool playerHasGoods = potentialOccupant.GetComponent<Meeple> ().player.GetComponent<PlayerInventory> ().hasGoods ();
 						return (base.CanOccupy (potentialOccupant) && playerHasGoods);
-				else
-						return (!goodToTradeForCarpet && potentialOccupant.CompareTag ("goodToken"));
+				} else
+						return (!goodToTradeForCarpet && potentialOccupant.GetComponent<GoodToken> ());
 		}
 	
 		public override void SetOccupant (GameObject o)
 		{
-				if (isMeeple (o))
-						base.SetOccupant (o);
-				else {
-						goodToTradeForCarpet = o;
-			Debug.Log ("called");
+				if (o) {
+						if (isMeeple (o)) {
+								base.SetOccupant (o);
+								Debug.Log ("meeple");
+						} else {
+								goodToTradeForCarpet = o;
+								Debug.Log ("good ");
+		
+						}
+
+						if (occupant && goodToTradeForCarpet) {
+								givePlayerMagicCarpetPowerForGood (occupant);
+								GameObject.Find ("GameController").GetComponent<GameController> ().getNextPlayer ();
+			
+			
+						}
 				}
 
-				if (occupant && goodToTradeForCarpet) {
-						MagicCarpet.playerWithMagicCarpet = o.GetComponent<Meeple> ().player;
-						//goodToTradeForCarpet.DestroyFOREVER!!!!!
-						GameObject.Find ("GameController").GetComponent<GameController> ().getNextPlayer ();
-			
-			
-				}
-		
-		
-		
 		}
 
+		void givePlayerMagicCarpetPowerForGood (GameObject occupantPlayer)
+		{      
+				MagicCarpet.playerWithMagicCarpet = occupantPlayer.GetComponent<Meeple> ().player;
+				DesertGenerator.GoodItem goodGiven = goodToTradeForCarpet.GetComponent<GoodToken> ().good;
+				occupant.GetComponent<Meeple> ().player.GetComponent<PlayerInventory> ().removeGoods (goodGiven, 1);
+				
+		Object.Destroy (goodToTradeForCarpet);
+		               
+		}
+	
 		bool isMeeple (GameObject potentialOccupant)
 		{
 				return potentialOccupant.GetComponent<Meeple> ();
