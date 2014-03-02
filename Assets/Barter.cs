d43de2ada9b92731ff292a975b953e2094947b34
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2014-03-02 14:23:34 -0800

    Good Tiles may be placed!

---
+++ Assets/Barter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barter : MonoBehaviour {
+
+	public void AttemptToCompleteBarter () {
+		//if there is a meeple, and two good tokens end the turn
+		}
+
+}
---
+++ Assets/BarterPlacement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarterPlacement : DropLocation {
+
+	void Start () {
+		
+	}
+	
+
+	protected override bool  CanOccupy (GameObject potentialOccupant)
+	{
+		print ("Tried");
+		return potentialOccupant.GetComponent<GoodToken>() ? true : false ;
+	}
+
+	public virtual void SetOccupant (GameObject o)
+	{
+		occupant = o;
+
+		if (o) {
+			GameObject parent = gameObject.transform.parent.gameObject;
+			parent.GetComponent<Barter>().AttemptToCompleteBarter();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
